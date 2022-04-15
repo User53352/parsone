64d33525bb8fff587299482309d6e669940e56bc
Max <42574081+User53352@users.noreply.github.com>
2022-04-15 16:18:42 +0300

    Update ParserOne.py

--- ParserOne.py
+++ ParserOne.py
@@ -1,7 +1,4 @@
-hp@hp-HP-2000-Notebook-PC:~$ python3
-Python 3.8.10 (default, Jun  2 2021, 10:49:15) 
-[GCC 9.4.0] on linux
-Type "help", "copyright", "credits" or "license" for more information.
+
 >>> import requests # импорт необходимых библиотек для добычи данных
 >>> from bs4 import BeautifulSoup 
 >>> url = "https://scrapingclub.com/exercise/detail_basic/" # ресурс для тренировок
